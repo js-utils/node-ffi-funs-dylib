80ade3e5c81c6247fd8a012b7440fd4c337c9d72
sanmu <578595193@qq.com>
2020-02-23 17:01:22 +0800

    Initial Commit

---
+++ node-ffi-funs-dylib/node_ffi_funs_dylib.h
@@ -0,0 +1,13 @@
+//
+//  node_ffi_funs_dylib.h
+//  node-ffi-funs-dylib
+//
+//  Created by sanmu on 2020/2/23.
+//  Copyright © 2020 sanmu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface node_ffi_funs_dylib : NSObject
+
+@end
---
+++ node-ffi-funs-dylib/node_ffi_funs_dylib.m
@@ -0,0 +1,13 @@
+//
+//  node_ffi_funs_dylib.m
+//  node-ffi-funs-dylib
+//
+//  Created by sanmu on 2020/2/23.
+//  Copyright © 2020 sanmu. All rights reserved.
+//
+
+#import "node_ffi_funs_dylib.h"
+
+@implementation node_ffi_funs_dylib
+
+@end
